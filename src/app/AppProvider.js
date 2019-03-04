f0d8ef1e653e626356e3eef515677a3d92dc69d8
michaelmenard <mlmmayhem@gmail.com>
2019-03-04 16:31:25 -0700

    click handler using the app context

---
+++ src/app/AppProvider.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export const AppContext = React.createContext();
+
+const MAX_FAVORITES = 10;
+const TIME_UNITS = 10;
+
+export class AppProvider extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            page: 'dashboard',
+            setPage: this.setPage
+        }
+    }
+
+
+    setPage = page => this.setState({page})
+
+    render(){
+        return (
+            <AppContext.Provider value={this.state}>
+                {this.props.children}
+            </AppContext.Provider>
+        )
+    }
+
+}
